9ec6cb5aa1ee78270372cf2cc90ce43d35db2003
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-25 12:15:53 +0500

    commit

--- signup/index.php
+++ signup/index.php
@@ -1,9 +1,12 @@
 <?php
 session_start();
 
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true && isset($_SESSION['role'])) {
 
-    if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin') {
+    if ($_SESSION['role'] == 'superadmin') {
+        header("Location: ../superadmin/");
+        exit;
+    } elseif ($_SESSION['role'] == 'admin') {
         header("Location: ../admin/");
         exit;
     } else {
