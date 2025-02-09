be3e265c17933e67c77a85e541bf6c88b015dcab
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-09 22:52:14 +0500

    commit

--- superadmin/index.php
+++ superadmin/index.php
@@ -1,121 +1,144 @@
 <?php
-session_start();
+$currentPage = basename($_SERVER['SCRIPT_NAME']);
 
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("Location: ../login/");
-    exit;
-}
+$menuItems = [
+    [
+        "menuTitle" => "Menu",
+        "icon" => "fas fa-home",
+        "pages" => [
+            ["title" => "Home", "url" => "index.php"]
+        ],
+    ],
+];
 
-if (isset($_SESSION['role']) && $_SESSION['role'] != 'superadmin') {
-    header("Location: ../login/");
-    exit;
-}
+$activePageInfo = array_reduce($menuItems, function ($carry, $menuItem) use ($currentPage) {
+    foreach ($menuItem['pages'] as $page) {
+        if ($currentPage === $page['url']) {
+            return [
+                "breadcrumbItems" => [
+                    ["title" => $menuItem['menuTitle'], "url" => "#"],
+                    ["title" => $page['title'], "url" => $page['url']]
+                ],
+                "pageTitle" => $page['title'],
+                "activeMenu" => $menuItem,
+                "activePage" => $page
+            ];
+        }
+    }
+    return $carry;
+}, null);
 
-include '../config.php';
-$query = new Database();
-
+$breadcrumbItems = $activePageInfo['breadcrumbItems'] ?? [];
+$pageTitle = $activePageInfo['pageTitle'] ?? '';
+$activeMenu = $activePageInfo['activeMenu'] ?? null;
+$activePage = $activePageInfo['activePage'] ?? null;
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<title><?= $pageTitle ?></title>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <link rel="stylesheet" href="../src/css/adminlte.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-</head>
-
-<body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-        <?php include './header.php' ?>
-        <div class="content-wrapper">
-
-            <section class="content">
-                <div class="container-fluid">
-
-                    <div class="row">
-                        <div class="col-lg-3 col-6">
-
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>150</h3>
-
-                                    <p>New Orders</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-bag"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-3 col-6">
-
-                            <div class="small-box bg-success">
-                                <div class="inner">
-                                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                                    <p>Bounce Rate</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-stats-bars"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-3 col-6">
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3>44</h3>
-
-                                    <p>User Registrations</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-person-add"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-3 col-6">
-
-                            <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>65</h3>
-
-                                    <p>Unique Visitors</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-
-            </section>
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" data-widget="pushmenu" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block"><a href="./" class="nav-link">Home</a></li>
+    </ul>
+    <form class="form-inline ml-3">
+        <div class="input-group input-group-sm">
+            <input class="form-control form-control-navbar" type="search" placeholder="Search" name="search">
+            <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit"><i class="fas fa-search"></i></button>
+            </div>
         </div>
+    </form>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown"><a class="nav-link" href="#messages"><i class="far fa-comments"></i><span
+                    class="badge badge-danger navbar-badge">2</span></a></li>
+        <li class="nav-item dropdown"><a class="nav-link" href="#notifications"><i class="far fa-bell"></i><span
+                    class="badge badge-warning navbar-badge">5</span></a></li>
+    </ul>
+</nav>
 
-        <?php include 'footer.php'; ?>
+<div class="main-header" style="padding: 0px 10px; background-color: #f4f6f9; border-bottom: none !important;">
+    <div class="content-header">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark"><?= $pageTitle ?></h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <?php foreach ($breadcrumbItems as $item): ?>
+                        <li class="breadcrumb-item <?= $item['url'] === '#' ? 'active' : '' ?>">
+                            <?= $item['url'] === '#' ? $item['title'] : "<a href='{$item['url']}'>{$item['title']}</a>" ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ol>
+            </div>
+        </div>
     </div>
+</div>
 
-    <!-- jQuery -->
-    <script src="../src/js/jquery.min.js"></script>
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="./" class="brand-link">
+        <img src="./src/images/logo.png" alt="Admin Panel Logo" class="brand-image img-circle elevation-3">
+        <span class="brand-text font-weight-light">Admin Panel</span>
+    </a>
+    <div class="sidebar">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image"><img src="./src/images/default.png" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info"><a href="./" class="d-block">Iqbolshoh Ilhomjonov</a></div>
+        </div>
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+                <?php foreach ($menuItems as $menuItem): ?>
+                    <li class="nav-item has-treeview <?= $menuItem === $activeMenu ? 'menu-open' : '' ?>">
+                        <a class="nav-link <?= $menuItem === $activeMenu ? 'active' : '' ?>" href="#">
+                            <i class="nav-icon <?= $menuItem['icon'] ?>"></i>
+                            <p><?= $menuItem['menuTitle'] ?>
+                                <?= !empty($menuItem['pages']) ? '<i class="right fas fa-angle-left"></i>' : '' ?>
+                            </p>
+                        </a>
+                        <?php if (!empty($menuItem['pages'])): ?>
+                            <ul class="nav nav-treeview">
+                                <?php foreach ($menuItem['pages'] as $page): ?>
+                                    <li class="nav-item">
+                                        <a href="<?= $page['url'] ?>" class="nav-link <?= $page === $activePage ? 'active' : '' ?>">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p><?= $page['title'] ?></p>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        <?php endif; ?>
+                    </li>
+                <?php endforeach; ?>
+                <li class="nav-item" onclick="logout()">
+                    <a href="javascript:void(0);" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>Logout</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
 
-    <!-- Bootstrap 4 -->
-    <script src="../src/js/bootstrap.bundle.min.js"></script>
-    <script src="../src/js/adminlte.min.js"></script>
-</body>
-
-</html>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+    function logout() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You will be logged out!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, log me out!'
+        }).then((result) => {
+            if (result.value) {
+                window.location.href = './logout/';
+            }
+        });
+    }
+</script>
